771aeddd9e1ddfa8dcebd02bb6ce7a24f9b35ade
David Svenson <davidsvenson@outlook.com>
2026-02-06 22:17:09 +0100

    components: introduce base page component.

---
+++ sambo/components.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import typing as t
+
+import htpy as h
+from django.middleware import csrf
+from django.templatetags.static import static
+if t.TYPE_CHECKING:
+    from django.http import HttpRequest
+
+
+def page(request: HttpRequest, *, title: str, main: h.Node, head: h.Node) -> h.Element:
+    return h.html(".wa-theme-awesome.wa-brand-blue.wa-cloak", lang="sv")[
+        h.head[
+            h.meta(charset="utf-8"),
+            h.meta(
+                name="viewport",
+                content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover",
+            ),
+            h.title[title],
+            h.style[
+                # Avoid Flash of Undefined Custom Elements (FOUCE):
+                # https://www.abeautifulsite.net/posts/flash-of-undefined-custom-elements/
+                ":not(:defined) { visibility: hidden; }"
+            ],
+            h.link(rel="stylesheet", href=static("vendor/webawesome/styles/webawesome.css")),
+            h.link(rel="stylesheet", href=static("vendor/webawesome/styles/themes/awesome.css")),
+            h.script(type="module", src=static("vendor/webawesome/webawesome.loader.js")),
+            h.script(defer=True, src=static("vendor/alpinejs.min.js")),
+            h.script(defer=True, src=static("vendor/htmx.min.js")),
+            head,
+        ],
+        h.body(hx_headers=f'{{"X-CSRFToken": "{csrf.get_token(request)}"}}', x_data="")[h.main(".wa-stack")[main]],
+    ]
--- sambo/todo/components.py
+++ sambo/todo/components.py
@@ -2,9 +2,10 @@ from collections.abc import Iterable
 
 import htpy as h
 from django.http import HttpRequest, HttpResponse
-from django.middleware import csrf
 from django.templatetags.static import static
 from django.urls import reverse
+
+from sambo.components import page
 
 from .models import CheckList, CheckListItem
 
@@ -40,58 +41,40 @@ def items(instance: CheckList) -> Iterable[h.Element]:
         yield h.li[item(i)]
 
 
-def check_list(request: HttpRequest, instance: CheckList) -> HttpResponse:
-    return HttpResponse(
-        h.html(".wa-theme-awesome.wa-brand-blue.wa-cloak", lang="sv")[
-            h.head[
-                h.meta(charset="utf-8"),
-                h.meta(
-                    name="viewport",
-                    content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover",
-                ),
-                h.title[instance.name.capitalize()],
-                h.style[
-                    # Avoid Flash of Undefined Custom Elements (FOUCE):
-                    # https://www.abeautifulsite.net/posts/flash-of-undefined-custom-elements/
-                    ":not(:defined) { visibility: hidden; }"
+def check_list(request: HttpRequest, instance: CheckList) -> h.Element:
+    return page(
+        request,
+        title=instance.name.capitalize(),
+        head=[
+            h.link(rel="stylesheet", href=static("todo/styles.css")),
+            h.script(src=static("todo/script.js")),
+        ],
+        main=[
+            h.section(".wa-card")[
+                h.h1(slot="header")[instance.name],
+                h.form(
+                    hx_post=reverse("check_list", args=[instance.identifier]),
+                    hx_trigger="submit,plus-clicked",
+                    hx_target="ol",
+                    hx_swap="afterbegin",
+                )[
+                    h.wa_input(
+                        {"@submit.outside": '$el.value = ""', "@plus-clicked": '$el.value = ""'},
+                        name="description",
+                        placeholder="Lägg till ny punkt",
+                        pill=True,
+                        size="medium",
+                        with_clear=True,
+                    )[
+                        h.wa_icon(
+                            {"@click": '$dispatch("plus-clicked")'},
+                            slot="start",
+                            name="plus",
+                            variant="solid",
+                        )
+                    ],
                 ],
-                h.link(rel="stylesheet", href=static("vendor/webawesome/styles/webawesome.css")),
-                h.link(rel="stylesheet", href=static("vendor/webawesome/styles/themes/awesome.css")),
-                h.link(rel="stylesheet", href=static("todo/styles.css")),
-                h.script(type="module", src=static("vendor/webawesome/webawesome.loader.js")),
-                h.script(defer=True, src=static("vendor/alpinejs.min.js")),
-                h.script(defer=True, src=static("vendor/htmx.min.js")),
-                h.script(src=static("todo/script.js")),
-            ],
-            h.body(hx_headers=f'{{"X-CSRFToken": "{csrf.get_token(request)}"}}', x_data="")[
-                h.main(".wa-stack")[
-                    h.section(".wa-card")[
-                        h.h1(slot="header")[instance.name],
-                        h.form(
-                            hx_post=reverse("check_list", args=[instance.identifier]),
-                            hx_trigger="submit,plus-clicked",
-                            hx_target="ol",
-                            hx_swap="afterbegin",
-                        )[
-                            h.wa_input(
-                                {"@submit.outside": '$el.value = ""', "@plus-clicked": '$el.value = ""'},
-                                name="description",
-                                placeholder="Lägg till ny punkt",
-                                pill=True,
-                                size="medium",
-                                with_clear=True,
-                            )[
-                                h.wa_icon(
-                                    {"@click": '$dispatch("plus-clicked")'},
-                                    slot="start",
-                                    name="plus",
-                                    variant="solid",
-                                )
-                            ],
-                        ],
-                        h.ol(".wa-stack.wa-gap-m")[items(instance)],
-                    ]
-                ]
-            ],
-        ]
+                h.ol(".wa-stack.wa-gap-m")[items(instance)],
+            ]
+        ],
     )
